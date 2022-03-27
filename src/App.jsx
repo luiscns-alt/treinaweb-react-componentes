d0e9a26ab4cadf3970649e616bdcb5b4d15ca23e
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 13:52:15 -0300

    feat: listing elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import MeuComponent from "./components/component";
-import Time from "./components/Time";
-import MyButton from "./components/MyButton";
+import PeopleList from "./components/PeopleList";
 
 function App() {
-    const dados = {
-        nome: "Maria",
-        sobrenome: "Souza",
-        idade: 20
-    }
     return (
             <div className="App">
-                <MyButton />
+                <PeopleList />
             </div>
     )
 }
