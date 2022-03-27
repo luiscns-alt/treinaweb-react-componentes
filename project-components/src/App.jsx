89956faaab76ae0847c165294e79ddcc41b83722
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 13:42:11 -0300

    feat: event at the project

--- project-components/src/App.jsx
+++ project-components/src/App.jsx
@@ -3,18 +3,27 @@ import './App.css'
 import VideoPlayer from "./components/VideoPlayer";
 import VideoList from "./components/VideoList";
 import VideoCinema from "./components/VideoCinema";
+import { VideoService } from "./services/VideoService";
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.selectVideo = this.selectVideo.bind(this);
         this.state = {
             videos: [],
-            selectedVideo: {
-                img: 'https://storage.googleapis.com/coverr-public/thumbnails/Albert-Dock.jpg',
-                name: 'Albert-Dock',
-                url: 'https://storage.coverr.co/videos/a1V2JYqeQW4tNzRy6KP902Gt00C2UscR8N?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6Ijg3NjdFMzIzRjlGQzEzN0E4QTAyIiwiaWF0IjoxNjQ4Mzg5NjIxfQ.hRpoyFR8h0Va7iRhM34S7RNAqIBg7uRBMtYnCYcMD98'
-            }
+            selectedVideo: {}
         }
+    }
+
+    async componentDidMount() {
+        const videos = await VideoService.list();
+        this.setState({ videos });
+
+        this.selectVideo(videos[0]);
+    }
+
+    selectVideo(video) {
+        this.setState({ selectedVideo: video });
     }
 
     render() {
