cb1754370a12dc55111057c6a9dc549e343fe76b
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 10:16:46 -0300

    feat: application components

---
+++ project-components/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import VideoPlayer from "./components/VideoPlayer";
+import VideoList from "./components/VideoList";
+
+function App() {
+    return (
+            <div className="App">
+                <VideoPlayer />
+                <VideoList />
+            </div>
+    )
+}
+
+export default App
---
+++ project-components/src/components/VideoCinema.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function VideoCinema() {
+    return (
+            <div className="video-cinema" />
+    );
+}
+
+export default VideoCinema;
