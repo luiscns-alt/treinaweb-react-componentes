7fbd5df0c1a9f1c91a732687e2b3bc552a6be87e
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 14:24:47 -0300

    feat: properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ClickList from "./components/ClickList";
 function App() {
     return (
             <div className="App">
-                <ClickList>
+                <ClickList number={123}>
                     <h1>Title</h1>
                     <div>Ola web developers!</div>
                 </ClickList>
