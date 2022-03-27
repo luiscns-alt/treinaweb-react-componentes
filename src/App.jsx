4f4731efe85049701fd10235815e1bdf449986e4
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 15:11:25 -0300

    feat: create portals

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import ClickList from "./components/ClickList";
-import ClickListItem from "./components/ClickListItem";
-import MyVideo from "./components/MyVideo";
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-    return (
-            <div className="App">
-                <MyVideo src="https://storage.coverr.co/videos/a1V2JYqeQW4tNzRy6KP902Gt00C2UscR8N?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6Ijg3NjdFMzIzRjlGQzEzN0E4QTAyIiwiaWF0IjoxNjQ4NDAyNDYxfQ.9_LwN8aIGdextyEdDmmtj7MpH6YxgxIXteapLPqn33w" />
-                <ClickList>
-                    <ClickListItem></ClickListItem>
-                    <ClickListItem></ClickListItem>
-                    <ClickListItem></ClickListItem>
-                </ClickList>
-            </div>
-    )
+import ClickList from './components/ClickList';
+import ClickListItem from './components/ClickListItem';
+import MyVideo from './components/MyVideo';
+import Time from './components/Time'
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.item1 = React.createRef();
+        this.item2 = React.createRef();
+
+        this.state = {
+            selectedItem: this.item1
+        }
+
+        this.toggleItem = this.toggleItem.bind(this);
+    }
+
+    toggleItem() {
+        this.setState(({ selectedItem }) => {
+            return {
+                selectedItem: (selectedItem === this.item1 ? this.item2 : this.item1)
+            }
+        })
+    }
+
+    render() {
+        return (
+                <div className="App">
+                    <div>
+                        <ul>
+                            <li ref={this.item1}></li>
+                            <li ref={this.item2}></li>
+                        </ul>
+                        <Time container={this.state.selectedItem.current} />
+                        <button onClick={this.toggleItem}>Toggle</button>
+                    </div>
+
+                    <MyVideo src="https://app.coverr.co/s3/mp4/Albert-Dock.mp4" />
+                    <ClickList>
+                        <ClickListItem></ClickListItem>
+                        <ClickListItem></ClickListItem>
+                        <ClickListItem></ClickListItem>
+                    </ClickList>
+                </div>
+        );
+    }
 }
 
-export default App
+export default App;
