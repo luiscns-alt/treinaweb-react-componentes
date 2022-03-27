df07315ec6ab5eb0d6d4c16ac0de2285119f02ba
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 14:00:39 -0300

    feat: function parameters

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 
 const myList = [
-    {id: '12', nome: "João", idade: 23 },
-    {id: '34', nome: "Maria", idade: 25 },
-    {id: '56', nome: "Paulo", idade: 32 },
-    {id: '78', nome: "Bruna", idade: 20 }
+    { id: 12, nome: "João", idade: 23 },
+    { id: 34, nome: "Maria", idade: 25 },
+    { id: 56, nome: "Paulo", idade: 32 },
+    { id: 78, nome: "Bruna", idade: 20 }
 ]
 
 class PeopleList extends Component {
+    sayMyName(person) {
+        alert(person.nome);
+    }
+
     render() {
         return (
                 <ul>
-                    {myList.map(person => <li key={person.id}>{person.nome} - {person.idade} anos</li>)}
+                    {myList.map(person =>
+                            <li onClick={this.sayMyName.bind(this, person)}
+                                key={person.id}>{person.nome} - {person.idade} anos</li>
+                    )
+                    }
                 </ul>
         );
     }
